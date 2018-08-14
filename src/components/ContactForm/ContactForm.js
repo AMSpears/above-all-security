29518990e13a77133ac975cbc6f4a3d48f7e2f09
Angie Spears <angie.spears809@gmail.com>
2018-08-13 20:35:55 -0400

    update contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,7 +4,7 @@ import "./ContactForm.css"
 class ContactForm extends React.Component {
     render(){
         return(
-            <form>
+            <form action= "" method = "POST">
                
                 <input type = "text" name= "name" placeholder= "First Name" required/><br/>
             
@@ -12,7 +12,7 @@ class ContactForm extends React.Component {
              
                 <input type = "text" name= "email" placeholder= "Email Address" required /><br/>
               
-                <input type = "text" name= "number" placeholder= "Phone Number"/><br/>
+                <input type = "text" name= "number" placeholder= "Phone Number" minLength= "9"/><br/>
              
                 <textarea name = "message" rows = "5" cols= "50" placeholder= "How can we help you?" required >How can we help you? </textarea><br/>
 
